de13ac31f0c1b6871d91522155f060c5b65862ba
rajanit_navapara9 <rajanitnavapara9999@gmail.com>
2024-08-30 12:22:45 +0530

    Fixed Build Error

--- app/Components/NewsFeed/PostCard2/PostCard2Header/PostCard2Header.tsx
+++ app/Components/NewsFeed/PostCard2/PostCard2Header/PostCard2Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import ClubwizeGreenLogo from '../../../../../public/img/clubwizeGreenLogo.png'
+import ClubwizeGreenLogo from '../../../../../public/img/ClubwizeGreenLogo.png'
 
 const PostCard2Header = () => {
   return (
