d2a9efee56af66ffd9ce36a17d18145271b41c17
Himanshu4252 <himanshuhddudhpuri4252@gmail.com>
2024-09-03 12:17:13 +0530

    fixed design issues

--- app/Components/searchPanel/SearchBar.tsx
+++ app/Components/searchPanel/SearchBar.tsx
@@ -165,7 +165,8 @@ const [inputValue, setInputValue] = useState('');
               <li><a href="">Menu Item 1</a></li>
               <li><a href="">Menu Item 2</a></li>
               <li><a href="">Menu Item 3</a></li>
-            </ul>):(null)}
+            </ul>)
+            :(null)}
       </div>
     </div>  
     </div>
