89327f0fba5305e9d81dfd88db0b36ac25a3a6db
Raghu3-3102 <raghvendrakumarpandey321@gmail.com>
2024-09-13 11:18:25 +0530

    solveds

--- app/Components/PageNavigation/PageNavigation.tsx
+++ app/Components/PageNavigation/PageNavigation.tsx
@@ -12,6 +12,7 @@ const PageNavigation = () => {
         <Link className="bg-gray-600 text-white p-2 px-4 rounded-lg" href="/Other/BookMarks"><li>Bookmarks</li></Link>
         <Link className="bg-gray-600 text-white p-2 px-4 rounded-lg" href="/main/Debate"><li>Debate</li></Link>
         <Link className="bg-gray-600 text-white p-2 px-4 rounded-lg" href="/main/ProfileFeed"><li>Profile</li></Link>
+        <Link className="bg-gray-600 text-white p-2 px-4 rounded-lg" href="/projects"><li>Projects</li></Link>
        
         {/* <a href=""><li></li></a> */}
         
