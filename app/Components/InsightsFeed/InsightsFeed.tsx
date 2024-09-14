c62b72cd5c8dd9b14c807a92b7bdf1c3377f7ae6
rajanit_navapara9 <rajanitnavapara9999@gmail.com>
2024-09-14 13:56:53 +0530

    make sidebar dynamic

---
+++ app/Components/InsightsFeed/InsightsFeed.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const InsightsFeed = () => {
+  return (
+    <div className='w-full h-[70vh] flex flex-col gap-4 justify-center items-center bg-white text-lg shadow-md text-gray-600'>
+        Insights Feed
+        <p className='text-base'>No Insights is there</p>
+    </div>
+  )
+}
+
+export default InsightsFeed
