bbeb50ae7fef40e712485e13054fd23c094f6a13
Naren <deadlyassassin2003@gmail.com>
2024-09-19 12:48:29 +0530

    Making profile responsive

--- app/Components/Module/Module.tsx
+++ app/Components/Module/Module.tsx
@@ -1,151 +1,137 @@
-'use client'
-import React, { useEffect, useState } from 'react'
-import module from './Module.module.css'
-import styles from './Module.module.css'
-import Team from '../Create_Team/Team'
-import ModuleBar from '../NewsFeed/ModuleBar/ModuleBar'
+"use client";
+import React, { useEffect, useState } from "react";
+import module from "./Module.module.css";
+import styles from "./Module.module.css";
+import Team from "../Create_Team/Team";
+import ModuleBar from "../NewsFeed/ModuleBar/ModuleBar";
 
-import SearchBar from '../searchPanel/SearchBar'
-import Side_Bar from '../Sub_Componet/Side_Bar'
-import Module_page from './modules/Module_page'
-import Model from './modules/Model/Model'
-import Prefrence from '@/app/Prefrence/Prefrence'
-import ProfileFeed from '../ProfileFeed/ProfileFeed'
-import Feed from '@/app/Feed/page'
-import FeedPage from '@/app/main/Feed/page'
-import ActivityCopm from '../My_Activity/Activity_comp/Activity_copm'
-import Actevity from '../Actevity/Actevity'
-import Nodes from './Nodes/Nodes'
-import Members from '../Create_Team/TeamPopup/Members'
-import Profile from '../Profile2/Profile'
-import MembersFeed from '../MembersFeed/MembersFeed'
-import Approvals from '../Approvals/Approvals'
-import InsightsFeed from '../InsightsFeed/InsightsFeed'
-
+import SearchBar from "../searchPanel/SearchBar";
+import Side_Bar from "../Sub_Componet/Side_Bar";
+import Module_page from "./modules/Module_page";
+import Model from "./modules/Model/Model";
+import Prefrence from "@/app/Prefrence/Prefrence";
+import ProfileFeed from "../ProfileFeed/ProfileFeed";
+import Feed from "@/app/Feed/page";
+import FeedPage from "@/app/main/Feed/page";
+import ActivityCopm from "../My_Activity/Activity_comp/Activity_copm";
+import Actevity from "../Actevity/Actevity";
+import Nodes from "./Nodes/Nodes";
+import Members from "../Create_Team/TeamPopup/Members";
+import Profile from "../Profile2/Profile";
+import MembersFeed from "../MembersFeed/MembersFeed";
+import Approvals from "../Approvals/Approvals";
+import InsightsFeed from "../InsightsFeed/InsightsFeed";
 
 function Module() {
+  const [isModelOpen, setIsModelOpen] = useState(false);
 
-  const [isModelOpen,setIsModelOpen] = useState(false)
+  const setCloseModel = () => {
+    setIsModelOpen(true);
+  };
 
-  const setCloseModel = ()=>{
-    setIsModelOpen(true)
-  }
-
-  const setOpenModel = ()=>{
-    setIsModelOpen(false)
-  }
+  const setOpenModel = () => {
+    setIsModelOpen(false);
+  };
 
   useEffect(() => {
     if (isModelOpen) {
-      document.body.style.overflow = '';  // Disable scrolling
+      document.body.style.overflow = ""; // Disable scrolling
     } else {
-      document.body.style.overflow = '';        // Re-enable scrolling
+      document.body.style.overflow = ""; // Re-enable scrolling
     }
 
     // Clean up when the component unmounts
     return () => {
-      document.body.style.overflow = '';        // Ensure it's always reset
+      document.body.style.overflow = ""; // Ensure it's always reset
     };
   }, [isModelOpen]);
 
+  const [compindex, setCompindex] = useState(0);
 
-  const [compindex,setCompindex] = useState(0);
+  const compchange = (ind: number) => {
+    setCompindex(ind);
+  };
 
-  const compchange = (ind:number)=>{
-
-    setCompindex(ind)
-  }
-
-  
-  const changecomps = ()=>{
-
+  const changecomps = () => {
     switch (compindex) {
       case 0:
-        return(<FeedPage />)
-         break;
+        return <FeedPage />;
+        break;
       case 1:
-        return(<Module_page setCloseModel={setCloseModel} />)
-         break;
+        return <Module_page setCloseModel={setCloseModel} />;
+        break;
       case 2:
-        return(<ProfileFeed />)
-         break;
+        return <ProfileFeed />;
+        break;
 
       case 3:
-         return(<Nodes />)
-         break;
+        return <Nodes />;
+        break;
 
       case 4:
-        return(<MembersFeed />)
+        return <MembersFeed />;
         break;
 
       case 5:
-        return(<Approvals />)
+        return <Approvals />;
         break;
 
       case 6:
-       return(<InsightsFeed />)
-       break;
+        return <InsightsFeed />;
+        break;
 
       case 7:
-        return(<Actevity  />)
+        return <Actevity />;
         break;
 
       case 8:
-        return(<Prefrence />)
+        return <Prefrence />;
         break;
-    
+
       default:
         break;
     }
-
-  }
-
+  };
 
   return (
-    <div className="flex gap-4 " >
-    <div style={{zIndex:'1000'}} >
-    <Side_Bar />  
-    </div>
-
-    {isModelOpen ? 
-     <div className={styles.model_cnt} style={{zIndex:"10"}} >
-        <Model  setOpenModel={setOpenModel} />
-     </div>
-     :" "
-  }
-    
-
-    <div className="flex flex-col w-full" >
-      <SearchBar />
-
-      <div className="flex flex-row justify-evenly " style={{zIndex:"1"}} >
-      <div className="w-2/12" >
-         <Profile  compchange={compchange} />
+    <div className="flex gap-4 ">
+      <div style={{ zIndex: "1000" }}>
+        <Side_Bar />
       </div>
-       
 
-        {/* Feed */}
-        <div className='flex flex-col  gap-4 h-screen bg-[url("/img/FeedBackground.png")] my-1 '>
-          <div className="w-full flex space-x-2">
-            <ModuleBar />
-             <Team />
-          </div>
-          <div className="w-full lg:w-8/12 ">
-            {changecomps()}
-          </div>
+      {isModelOpen ? (
+        <div className={styles.model_cnt} style={{ zIndex: "10" }}>
+          <Model setOpenModel={setOpenModel} />
         </div>
+      ) : (
+        " "
+      )}
 
-        {/* Team */}
-        {/* <div className="w-3/12 ">
+      <div className="flex flex-col w-full">
+        <SearchBar />
+
+        <div className="flex flex-row justify-evenly " style={{ zIndex: "1" }}>
+          <div className="w-2/12">
+            <Profile compchange={compchange} />
+          </div>
+
+          {/* Feed */}
+          <div className='w-fit flex flex-col  gap-4 h-screen bg-[url("/img/FeedBackground.png")] my-1 '>
+            <div className="w-full flex space-x-2">
+              <ModuleBar />
+              <Team />
+            </div>
+            <div className="w-full lg:w-8/12 ">{changecomps()}</div>
+          </div>
+
+          {/* Team */}
+          {/* <div className="w-3/12 ">
          
         </div> */}
+        </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default Module
-
-
-
+export default Module;
--- app/Components/Profile2/Profile.tsx
+++ app/Components/Profile2/Profile.tsx
@@ -1,117 +1,96 @@
-import React, { useState } from 'react'
-import styles from './Profile.module.css'
-import Image from 'next/image'
-import backG from './Assets/image 1.png'
-import profile from './Assets/Pfotile pic (1).png'
-import buttum from './Assets/button.png'
-import pin from './Assets/button (1).png'
-import img_text from './Assets/Frame 1010105932.jpg'
-import img_ from './Assets/Frame 1010106209.png'
-import icon from './Assets/Icon ionic-ios-arrow-forward (1).png'
-import group1 from './Assets/Group.png'
-import group2 from './Assets/Group 633647.png'
-import group3 from './Assets/Group 633649.png'
-import group4 from './Assets/Group 633674.png'
-import group5 from './Assets/Group 633676.png'
-import group6 from './Assets/Group 633729.png'
-import group7 from './Assets/Group 633864.png'
-import group8 from './Assets/Group (1).png'
-import group9 from './Assets/Group 633674.png'
-
+"use client"
+import React, { useState } from "react";
+import Image from "next/image";
+import backG from "./Assets/image 1.png";
+import profile from "./Assets/Pfotile pic (1).png";
+import buttum from "./Assets/button.png";
+import pin from "./Assets/button (1).png";
+import img_text from "./Assets/Frame 1010105932.jpg";
+import img_ from "./Assets/Frame 1010106209.png";
+import icon from "./Assets/Icon ionic-ios-arrow-forward (1).png";
+import group1 from "./Assets/Group.png";
+import group2 from "./Assets/Group 633647.png";
+import group3 from "./Assets/Group 633649.png";
+import group4 from "./Assets/Group 633674.png";
+import group5 from "./Assets/Group 633676.png";
+import group6 from "./Assets/Group 633729.png";
+import group7 from "./Assets/Group 633864.png";
+import group8 from "./Assets/Group (1).png";
+import group9 from "./Assets/Group 633674.png";
 
 interface ChildComponentProps {
-    compchange: (ind: number) => void;
+  compchange: (ind: number) => void;
 }
 
+const Profile: React.FC<ChildComponentProps> = ({ compchange }) => {
+  const menuItemsData = [
+    { menuName: "News Feed", img: group1 },
+    { menuName: "Module", img: group2 },
+    { menuName: "Profile", img: group3 },
+    { menuName: "Chapters", img: group4 },
+    { menuName: "Members", img: group5 },
+    { menuName: "Approval", img: group6 },
+    { menuName: "Insights", img: group7 },
+    { menuName: "Activity", img: group8 },
+    { menuName: "Performance", img: group9 },
+  ];
 
+  const [activeIndex, setActiveIndex] = useState<number | null>(0);
 
-const  Profile :  React.FC<ChildComponentProps> = ({ compchange }) => {
-
-    const menuItemsData = [
-        { menuName: "News Feed", img: group1 },
-        { menuName: "Module", img: group2 },
-        { menuName: "Profile", img: group3 },
-        { menuName: "Chapters", img: group4 },
-        { menuName: "Members", img: group5 },
-        { menuName: "Approval", img: group6 },
-        { menuName: "Insights", img: group7 },
-        { menuName: "Activity", img: group8 },
-        { menuName: "Performance", img: group9 },
-      ];
-
-      const [activeIndex, setActiveIndex] = useState<number | null>(0);// to track which menu is active
-
-      const handleClick = (index:number) => {
-        setActiveIndex(index); // Set the active menu item by index
-        compchange(index)
-      };
-
-
-
+  const handleClick = (index: number) => {
+    setActiveIndex(index);
+    compchange(index);
+  };
 
   return (
-    <div className='sticky top-[8vh]'>
-    <div className={styles.profile_card}>
-        <div className={styles.background_img_cnt}>
-            <Image src={backG} alt='images' />
-        </div>  
-        <div className={styles.profile_pic_cnt}>
-            <div className={styles.main_cnt}>
-                <Image src={profile} alt='Images' />
-            </div>
-            <div className={styles.btn_cnt}>
-           
-           <button className={styles.btn}><Image src={buttum} alt='Images' /></button>
-           <button className={styles.btn}><Image src={pin} alt='Images' /></button>
-
-           </div>
-           
-          
-
-           
-
+    <div className="sticky w-full">
+      <div className="w-auto mt-5 -ml-7 bg-white rounded-[10px] shadow-sm sticky top-[80px] z-[-1]">
+        <div className="w-full h-[124px] rounded-t-[10px]">
+          <Image src={backG} alt="Background Image" className="rounded-t-[10px]" />
         </div>
-        <div className={styles.text_cnt_profile}>
-            <div className={styles.haid_cnt}>
-            <Image src={img_text} alt='Images' />
-            </div>
-            <button className={styles.joining_btn}>Joined</button>
-
-            <div className={styles.text_cnt_box}>
-            
-
-          
-             {menuItemsData.map((item, index) => (
-        <div
-          key={index}
-          onClick={() => handleClick(index)}
-          className={`${styles.textBox} ${
-            activeIndex === index ? `${styles.bg_color} ${styles.border_green}` : ""
-          }`}
-        >
-          <div className={styles.img_cnt}>
-            <Image src={item.img} alt={item.menuName} />
+        <div className="relative -mt-[40px] flex justify-between items-end pr-[5px]">
+          <div className="ml-[10%] w-[30%]">
+            <Image src={profile} alt="Profile Image" />
           </div>
-          <p>{item.menuName}</p>
-          <div className={styles.arrow_left}>
-            <Image src={icon} alt="arrow" />
+          <div className="flex justify-between w-[80%] items-end h-[50px] ml-[10px]">
+            <button className="w-[60px] h-[25px]">
+              <Image src={buttum} alt="Button Image" />
+            </button>
+            <button className="w-[60px] h-[25px]">
+              <Image src={pin} alt="Pin Image" />
+            </button>
           </div>
         </div>
-      ))}
-             
-
-             
-           
-             
-            </div>
+        <div className="mt-[18%] w-full h-[75%] px-[10%] py-2">
+          <div className="w-full h-[18%]">
+            <Image src={img_text} alt="Text Image" />
+          </div>
+          <button className="w-full h-[30px] rounded-[10px] my-[18px] border border-gray-400 text-gray-500 flex items-center justify-center transition-all hover:text-gray-200 hover:bg-black">
+            Joined
+          </button>
+          <div className="w-full h-[70%] flex flex-col gap-2 py-[5px]">
+            {menuItemsData.map((item, index) => (
+              <div
+                key={index}
+                onClick={() => handleClick(index)}
+                className={`w-full h-[33px] flex items-start rounded-[8px] p-[5px] relative cursor-pointer ${
+                  activeIndex === index ? "border border-green-500 bg-green-200" : ""
+                }`}
+              >
+                <div className="w-[30px] h-full flex items-center justify-center rounded-[5px]">
+                  <Image src={item.img} alt={item.menuName} />
+                </div>
+                <p className="text-black">{item.menuName}</p>
+                <div className="absolute right-[10px] flex items-center justify-center w-[20px]">
+                  <Image src={icon} alt="Arrow Icon" />
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-
-       
-
+      </div>
     </div>
-      
-    </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
