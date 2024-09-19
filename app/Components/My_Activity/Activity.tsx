7dd3451855b1a7507b10e3e92db69fb947bebeb1
Naren <deadlyassassin2003@gmail.com>
2024-09-20 02:15:00 +0530

    Making Activity_copm responsive

--- app/Components/My_Activity/Activity.tsx
+++ app/Components/My_Activity/Activity.tsx
@@ -1,57 +1,39 @@
-"use client"
+"use client";
 
-import styles from '../Sub_Componet/Side_Bar.module.css'
+import styles from "../Sub_Componet/Side_Bar.module.css";
 
-import Side_Bar from '../Sub_Componet/Side_Bar'
-import SearchBar from '../searchPanel/SearchBar'
+import Side_Bar from "../Sub_Componet/Side_Bar";
+import SearchBar from "../searchPanel/SearchBar";
 
-import Chat from '../Setting/Chat/Chat'
-import Activity_copm from './Activity_comp/Activity_copm'
-
-
-
-
-
-
+import Chat from "../Setting/Chat/Chat";
+import Activity_copm from "./Activity_comp/Activity_copm";
 
 const Activity = () => {
-  return(
-<>
+  return (
+    <>
+      <div className="flex gap-1" style={{ zIndex: "1000" }}>
+        {/* Sidebar */}
+        <div className="">
+          <Side_Bar />
+        </div>
 
-<div className="flex gap-1" style={{ zIndex: '1000' }}>
-    {/* Sidebar */}
-    <div className="sticky top-0 z-50" >
-     <Side_Bar />
-     
-    </div>
-  
-    {/* Main Content Area */}
-    <div className="flex flex-col w-full">
-      <SearchBar />
-  
-      <div className="flex flex-row justify-evenly">
-        {/* Setting Sidebar */}
-  
-        {/* Main Feed Area */}
-        <div className="flex flex-col w-6/12 gap-2 items-center h-screen bg-[url('/img/FeedBackground.png')] my-1">
-         
-          <div className={styles.comp_bar}>
-           <Activity_copm />
+        {/* Main Content Area */}
+        <div className="flex flex-col w-full">
+          <SearchBar />
+
+          <div className="flex flex-row gap-5">
+            <div className="flex flex-col w-8/12 items-center h-screen my-1">
+                <Activity_copm />
+            </div>
+
+            <div className="w-4/12">
+              <Chat />
+            </div>
           </div>
         </div>
-  
-        {/* Team Section */}
-        <div className="w-2/12">
-          <Chat />
-        </div>
       </div>
-    </div>
-  </div>
+    </>
+  );
+};
 
-
-</>    
-     
-  )
-}
-
-export default Activity
+export default Activity;
