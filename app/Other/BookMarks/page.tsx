3b2bacebd84205115146de11f46e2b8456642f50
arfahmanaal <arfahmanaal@gmail.com>
2024-09-20 07:13:53 +0530

    made bookmarks page responsive

--- app/Other/BookMarks/page.tsx
+++ app/Other/BookMarks/page.tsx
@@ -1,14 +1,64 @@
+'use client'
+import React,{useState, useEffect} from 'react';
 import Bookmarks from "@/app/Components/MyItems/Bookmarks";
-import Message from "../Message/page";
+import MessageList from '@/app/Components/Messages/MessageList';
+import Chat from '@/app/Components/Messages/Chat';
+import { contacts, chatMessages } from '@/app/Other/Message/page';
 const BookMarks = () => {
+  const [selectedContact, setSelectedContact] = useState<number | null>(null);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+  const handleSelectContact = (id: number) => {
+    setSelectedContact(id);
+  };
+
+  const handleBackButtonClick = () => {
+    setSelectedContact(null);
+  };
+
+  const selectedContactDetails = contacts.find((contact) => contact.id === selectedContact);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 2400);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
     return(
         <div className="flex h-screen">
         <div className="flex-3 bg-white h-screen rounded-lg">
           <Bookmarks />
         </div>
-        <div className="flex-1 p-4">
-          <Message />
+        
+        <div className="flex-1 p-4 ">
+      {isMobile && selectedContact !== null ? (
+        <div className="relative flex-grow">
+          <button
+            onClick={handleBackButtonClick}
+            className="absolute top-3 left-0 p-0 text-lg ml-0 bg-gray-100 border font-bold"
+          >
+            &lt;
+          </button>
+          {selectedContactDetails && (
+            <Chat messages={chatMessages[selectedContact] || []} contact={selectedContactDetails} />
+          )}
         </div>
+      ) : (
+       
+        <>
+          <div className="flex-grow">
+            <MessageList contacts={contacts} onSelectContact={handleSelectContact} />
+          </div>
+          {selectedContact !== null && selectedContactDetails && (
+            <div className="flex-grow">
+              <Chat messages={chatMessages[selectedContact] || []} contact={selectedContactDetails} />
+            </div>
+          )}
+        </>
+      )}
+    </div>
       </div>
       
        
--- app/Other/Message/page.tsx
+++ app/Other/Message/page.tsx
@@ -1,93 +1,92 @@
 'use client';
-
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import MessageList from '@/app/Components/Messages/MessageList';
 import Chat from '@/app/Components/Messages/Chat';
 
+const contacts = [
+  { id: 1, name: 'Floyd Miles', lastMessage: 'Thanks', avatar: '/MsgImages/avatar1.png' },
+  { id: 2, name: 'Arlene McCoy', lastMessage: 'May we go for the lunch?', avatar: '/MsgImages/avatar2.png' },
+  { id: 3, name: 'Albert Flores', lastMessage: 'You: Yeah good man👨🏻‍💻', avatar: '/MsgImages/avatar3.png' },
+  { id: 4, name: 'Brooklyn Simmons', lastMessage: 'Fine hahah 😂', avatar: '/MsgImages/avatar4.png' },
+  { id: 5, name: 'Ronald Richards', lastMessage: 'Good work!!', avatar: '/MsgImages/avatar5.png' },
+  { id: 6, name: 'Robert Fox', lastMessage: 'You: Need some help brother!!', avatar: '/MsgImages/avatar6.png' },
+  { id: 7, name: 'Courtney Henry', lastMessage: 'You: @Courtney', avatar: '/MsgImages/avatar7.png' },
+  { id: 8, name: 'Emily Johnson', lastMessage: 'See you soon!', avatar: '/MsgImages/avatar8.png' }, 
+];
+
+const chatMessages: { [key: number]: Array<{ sender: string; text: string; timestamp: string }> } =  {
+  1: [
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet🤴 consectetur.', timestamp: 'Yesterday, 8:53 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:10 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet', timestamp: 'Yesterday, 9:15 PM' },
+    { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:20 PM' },
+    { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consect.', timestamp: 'Yesterday, 9:25 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit😂 amet consectetur.', timestamp: 'Today, 9:35 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet✨ consectetur.', timestamp: 'Today, 9:35 PM' },
+  ],
+  2: [
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet🤴 consectetur.', timestamp: 'Yesterday, 8:53 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:10 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet', timestamp: 'Yesterday, 9:15 PM' },
+    { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:20 PM' },
+    { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consect.', timestamp: 'Yesterday, 9:25 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit😂 amet consectetur.', timestamp: 'Today, 9:35 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet✨ consectetur.', timestamp: 'Today, 9:35 PM' },
+  ],
+  3: [
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet🤴 consectetur.', timestamp: 'Yesterday, 8:53 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:10 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet', timestamp: 'Yesterday, 9:15 PM' },
+    { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:20 PM' },
+    { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consect.', timestamp: 'Yesterday, 9:25 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit😂 amet consectetur.', timestamp: 'Today, 9:35 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet✨ consectetur.', timestamp: 'Today, 9:35 PM' },
+  ],
+  4: [
+    { sender: 'Brooklyn Simmons', text: 'Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 6:15 PM' },
+    { sender: 'me', text:'Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 11:00 PM' },
+  ],
+  5: [
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet🤴 consectetur.', timestamp: 'Yesterday, 8:53 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:10 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet', timestamp: 'Yesterday, 9:15 PM' },
+    { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:20 PM' },
+    { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consect.', timestamp: 'Yesterday, 9:25 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit😂 amet consectetur.', timestamp: 'Today, 9:35 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet✨ consectetur.', timestamp: 'Today, 9:35 PM' },
+  ],
+  6: [
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet🤴 consectetur.', timestamp: 'Yesterday, 8:53 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:10 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet', timestamp: 'Yesterday, 9:15 PM' },
+    { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:20 PM' },
+    { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consect.', timestamp: 'Yesterday, 9:25 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit😂 amet consectetur.', timestamp: 'Today, 9:35 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet✨ consectetur.', timestamp: 'Today, 9:35 PM' },
+  ],
+  7: [
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet🤴 consectetur.', timestamp: 'Yesterday, 8:53 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:10 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet', timestamp: 'Yesterday, 9:15 PM' },
+    { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:20 PM' },
+    { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consect.', timestamp: 'Yesterday, 9:25 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit😂 amet consectetur.', timestamp: 'Today, 9:35 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet✨ consectetur.', timestamp: 'Today, 9:35 PM' },
+  ],
+  8: [
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet🤴 consectetur.', timestamp: 'Yesterday, 8:53 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:10 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet', timestamp: 'Yesterday, 9:15 PM' },
+    { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:20 PM' },
+    { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consect.', timestamp: 'Yesterday, 9:25 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit😂 amet consectetur.', timestamp: 'Today, 9:35 PM' },
+    { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet✨ consectetur.', timestamp: 'Today, 9:35 PM' },
+  ],
+};
+
+
 const Message = () => {
   const [selectedContact, setSelectedContact] = useState<number | null>(null);
-  
-  const contacts = [
-    { id: 1, name: 'Floyd Miles', lastMessage: 'Thanks', avatar: '/MsgImages/avatar1.png' },
-    { id: 2, name: 'Arlene McCoy', lastMessage: 'May we go for the lunch?', avatar: '/MsgImages/avatar2.png' },
-    { id: 3, name: 'Albert Flores', lastMessage: 'You: Yeah good man👨🏻‍💻', avatar: '/MsgImages/avatar3.png' },
-    { id: 4, name: 'Brooklyn Simmons', lastMessage: 'Fine hahah 😂', avatar: '/MsgImages/avatar4.png' },
-    { id: 5, name: 'Ronald Richards', lastMessage: 'Good work!!', avatar: '/MsgImages/avatar5.png' },
-    { id: 6, name: 'Robert Fox', lastMessage: 'You: Need some help brother!!', avatar: '/MsgImages/avatar6.png' },
-    { id: 7, name: 'Courtney Henry', lastMessage: 'You: @Courtney', avatar: '/MsgImages/avatar7.png' },
-    { id: 8, name: 'Emily Johnson', lastMessage: 'See you soon!', avatar: '/MsgImages/avatar8.png' }, 
-  ];
-  
-  const chatMessages: { [key: number]: Array<{ sender: string; text: string; timestamp: string }> } =  {
-    1: [
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet🤴 consectetur.', timestamp: 'Yesterday, 8:53 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:10 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet', timestamp: 'Yesterday, 9:15 PM' },
-      { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:20 PM' },
-      { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consect.', timestamp: 'Yesterday, 9:25 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit😂 amet consectetur.', timestamp: 'Today, 9:35 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet✨ consectetur.', timestamp: 'Today, 9:35 PM' },
-    ],
-    2: [
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet🤴 consectetur.', timestamp: 'Yesterday, 8:53 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:10 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet', timestamp: 'Yesterday, 9:15 PM' },
-      { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:20 PM' },
-      { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consect.', timestamp: 'Yesterday, 9:25 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit😂 amet consectetur.', timestamp: 'Today, 9:35 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet✨ consectetur.', timestamp: 'Today, 9:35 PM' },
-    ],
-    3: [
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet🤴 consectetur.', timestamp: 'Yesterday, 8:53 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:10 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet', timestamp: 'Yesterday, 9:15 PM' },
-      { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:20 PM' },
-      { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consect.', timestamp: 'Yesterday, 9:25 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit😂 amet consectetur.', timestamp: 'Today, 9:35 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet✨ consectetur.', timestamp: 'Today, 9:35 PM' },
-    ],
-    4: [
-      { sender: 'Brooklyn Simmons', text: 'Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 6:15 PM' },
-      { sender: 'me', text:'Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 11:00 PM' },
-    ],
-    5: [
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet🤴 consectetur.', timestamp: 'Yesterday, 8:53 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:10 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet', timestamp: 'Yesterday, 9:15 PM' },
-      { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:20 PM' },
-      { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consect.', timestamp: 'Yesterday, 9:25 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit😂 amet consectetur.', timestamp: 'Today, 9:35 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet✨ consectetur.', timestamp: 'Today, 9:35 PM' },
-    ],
-    6: [
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet🤴 consectetur.', timestamp: 'Yesterday, 8:53 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:10 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet', timestamp: 'Yesterday, 9:15 PM' },
-      { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:20 PM' },
-      { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consect.', timestamp: 'Yesterday, 9:25 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit😂 amet consectetur.', timestamp: 'Today, 9:35 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet✨ consectetur.', timestamp: 'Today, 9:35 PM' },
-    ],
-    7: [
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet🤴 consectetur.', timestamp: 'Yesterday, 8:53 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:10 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet', timestamp: 'Yesterday, 9:15 PM' },
-      { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:20 PM' },
-      { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consect.', timestamp: 'Yesterday, 9:25 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit😂 amet consectetur.', timestamp: 'Today, 9:35 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet✨ consectetur.', timestamp: 'Today, 9:35 PM' },
-    ],
-    8: [
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet🤴 consectetur.', timestamp: 'Yesterday, 8:53 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:10 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet', timestamp: 'Yesterday, 9:15 PM' },
-      { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur.', timestamp: 'Yesterday, 9:20 PM' },
-      { sender: 'me', text: 'Lorem ipsum dolor Lorem ipsum dolor sit amet consect.', timestamp: 'Yesterday, 9:25 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit😂 amet consectetur.', timestamp: 'Today, 9:35 PM' },
-      { sender: 'Arlene McCoy', text: 'Lorem ipsum dolor sit amet✨ consectetur.', timestamp: 'Today, 9:35 PM' },
-    ],
-  };
-  
   const handleSelectContact = (id: number) => {
     setSelectedContact(id);
   };
@@ -106,5 +105,7 @@ const Message = () => {
     </div>
   );
 };
+export { contacts, chatMessages };
+
 
 export default Message;
